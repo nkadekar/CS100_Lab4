5627776ec7af0ecefee1bc2444f23f0d6d5d36df
nkadekar <nkadekar7@gmail.com>
2020-11-01 09:39:29 -0800

    Stash Changes

--- op_test.hpp
+++ op_test.hpp
@@ -4,10 +4,26 @@
 #include "gtest/gtest.h"
 
 #include "op.hpp"
+#include "div.hpp"
+
 
 TEST(OpTest, OpEvaluateNonZero) {
     Op* test = new Op(8);
     EXPECT_EQ(test->evaluate(), 8);
 }
 
-#endif //__OP_TEST_HPP__
+TEST(DivTest, DivEvaluateNonZero) {
+    Op* test = new Op(4); Op* test2 = new Op(5); Base* d = new Div(test, test2);
+    EXPECT_NEAR(d->evaluate(), 20, .001);
+}
+
+TEST(DivTest, DivEvaluateZero) {
+    Op* test = new Op(0); Op* test2 = new Op(0); Base* d = new Div(test, test2);
+    EXPECT_NEAR(d->evaluate(), 0, .001);
+}
+
+TEST(DivTest, DivEvaluateNegative) {
+	Op* test = new Op(-8); Op* test2 = new Op(0.5); Base* d = new Div(test, test2);
+	EXPECT_NEAR(d->evaluate(), -4, .001)
+}
+
