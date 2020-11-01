873060e53c35d47870ae6c7d210698c8844de32b
nkadekar <nkadekar7@gmail.com>
2020-11-01 10:44:20 -0800

    Adding div.hpp file

---
+++ div.hpp
@@ -0,0 +1,26 @@
+#ifndef __DIV_HPP__
+#define __DIV_HPP__
+
+#include "base.hpp"
+#include "op.hpp"
+#include <string>
+
+using namespace std;
+
+class Div : public Base {
+   private:
+        Base* leftChild = nullptr;
+		Base* rightChild = nullptr;
+   public:
+   		Div(Base* leftChild, Base* rightChild) {
+		    this->leftChild = leftChild;
+			this->rightChild = rightChild;
+		}
+		virtual double evaluate() { return leftChild->evaluate() / rightChild->evaluate(); }
+		virtual std::string stringify() {
+			ostringstream inSS;
+			inSS << setprecision(5) << leftChild->evaluate() << " / " << rightChild->evaluate();
+			return inSS.str(); }
+};
+
+#endif //__DIV_HPP__
